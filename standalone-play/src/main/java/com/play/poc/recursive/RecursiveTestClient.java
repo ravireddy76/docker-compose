044535cd509c5e2e165142fbdba092d1a6de0a2d
ravipocworld20 <ravipocworld20@gmail.com>
2020-06-15 22:22:39 -0500

    Fixed code changes for domian model with table properties

--- standalone-play/src/main/java/com/play/poc/recursive/RecursiveTestClient.java
+++ standalone-play/src/main/java/com/play/poc/recursive/RecursiveTestClient.java
@@ -1,4 +1,45 @@
 package com.play.poc.recursive;
 
+import java.util.*;
+
 public class RecursiveTestClient {
+
+    public static void main(String args[]) {
+        try {
+            process();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static void process() {
+        Map<String, Integer> resultMap = new HashMap<>();
+        int dataCounter = 0;
+        Map<String, Integer> results = getFirstCall(resultMap, dataCounter);
+        System.out.println("results :: "+results.toString());
+    }
+
+    private static Map<String, Integer> getFirstCall(Map<String, Integer> resultMap, int dataCounter) {
+        int randomSize = randomDataSize();
+        System.out.println(">> Random Size :: "+randomSize+",,  Data Counter :: "+dataCounter);
+
+        if(randomSize < 1000) {
+            resultMap.put("Counter".concat(String.valueOf(dataCounter)), randomSize);
+            return resultMap;
+        }else{
+            resultMap.put("Counter".concat(String.valueOf(dataCounter)), randomSize);
+            dataCounter++;
+            return getFirstCall(resultMap, dataCounter);
+        }
+
+//        resultMap.put("Counter".concat(String.valueOf(counter)), randomSize);
+//        return getFirstCall();
+    }
+
+    private static int randomDataSize() {
+        List<Integer> givenList = Arrays.asList(1000, 1000, 3);
+        Random rand = new Random();
+        int randomSize = givenList.get(rand.nextInt(givenList.size()));
+        return randomSize;
+    }
 }
